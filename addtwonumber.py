1d1fb6ba7426ea926ce4675775c74dcd71367891
Ka1 <135298514+HungKai30@users.noreply.github.com>
2025-02-06 15:19:32 +0700

    go
    Update bai 2

---
+++ addtwonumber.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    return a + b
+
+# Example usage
+if __name__ == "__main__":
+    num1 = float(input("Nhap so thu nhat vao: "))
+    num2 = float(input("Nhap so thu hai vao: "))
+    result = add_two_numbers(num1, num2)
+    print(f"Tong cua 2 so la {result}")
